5684f23e76dfb1999f9b373f138eeb2395c017d6
shawn-skt <2857043555@qq.com>
2021-12-22 18:50:52 +0800

    shellscripts updated

--- fix_err.sh
+++ fix_err.sh
@@ -10,13 +10,13 @@ function fix_err(){
         else
             filename=${file%.*}
             sed -i '' "s/&#39;/'/g" $1/$filename
-            solc --combined-json abi,bin,bin-runtime,srcmap,srcmap-runtime,ast $1/$filename > $1/json/fix/$filename.json
+            solc --combined-json abi,bin,bin-runtime,srcmap,srcmap-runtime,ast $1/$filename > $1/json/fixed/$filename.json
 
             if [ $? -ne 0 ]; then
                 echo "parsing ${filename} error"
-                mv $1/json/error/$filename.json $1/json/fail2fix/$filename.json
+                mv $1/$filename $1/error/$filename
             else
-                echo "parsing ${file_name} successfully"
+                echo "parsing ${filename} successfully"
                 rm -rf $1/json/error/${file}
             fi
         fi
